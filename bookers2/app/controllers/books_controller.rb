d4454ca51f20a43e17a828e927bb3dd88baec247
yonePC <ys.mailaddress0929@gmail.com>
2021-06-20 08:09:57 +0000

    [update]kadai3-29

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -2,34 +2,37 @@ class BooksController < ApplicationController
 
 def index
   @books = Book.all
-  
+
 end
-  
+
 def show
-  
+
 end
 
 def create
-  
-  
+  @books = Book.new(book_image_params)
+  @books.user_id = current_user.id
+  @book.save
+  redirest_to books_path
+
 end
 
 def edit
-  
+
 end
 
 def update
-  
+
 end
 
 def destroy
-  
+
 end
 
 
 private
   def book_params
-    params.require(:book).permit(:title, :opinion, :image)  
+    params.require(:book).permit(:title, :opinion, :image)
   end
 
 end
