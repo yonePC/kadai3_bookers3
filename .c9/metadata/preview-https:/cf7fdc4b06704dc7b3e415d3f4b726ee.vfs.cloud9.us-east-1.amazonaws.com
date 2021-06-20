b029616c704438a76ad43a98528c09bcf5052c6a
yonePC <ys.mailaddress0929@gmail.com>
2021-06-20 01:12:15 +0000

    [update]kadai3 ログイン画面を編集

---
+++ .c9/metadata/preview-https:/cf7fdc4b06704dc7b3e415d3f4b726ee.vfs.cloud9.us-east-1.amazonaws.com
@@ -0,0 +1,1 @@
+{"changed":false,"filter":false,"title":"[P] https://cf7fdc4b06704dc7b3e415d3f4b726ee.vfs.cloud9.us-east-1.amazonaws.com","value":"","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://cf7fdc4b06704dc7b3e415d3f4b726ee.vfs.cloud9.us-east-1.amazonaws.com","previewer":"previewBrowser","disableInjection":true},"timestamp":1624142245055}
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
+end
+
+  resources :users, only: [:show]
+  
 end
