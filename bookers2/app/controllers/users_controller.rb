64efbc9732f183f014f2a650c302243f5efdcadb
yonePC <ys.mailaddress0929@gmail.com>
2021-06-22 14:42:22 +0000

    [update] introductionとimage_idのカラム追加

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :encrypted_password, :introduction)
+    params.require(:user).permit(:name, :email, :encrypted_password, :introduction, :image)
   end
 
 end
