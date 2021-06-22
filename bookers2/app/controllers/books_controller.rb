fa8e85ea0f61f141396a9b27436faeb33e6a210d
yonePC <ys.mailaddress0929@gmail.com>
2021-06-22 12:28:52 +0000

    [update]編集と削除を追加

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -2,23 +2,29 @@ class BooksController < ApplicationController
 
 def index
   @books = Book.all
-  #@book = Book.new
+  @book = Book.new
 
 
 end
 
 def show
   @book = Book.find(params[:id])
-  @book = Book.new
+  @user = @book.user
+  @newbook = Book.new
 end
 
 def create
-  @books = Book.new(book_params)
-  @books.user_id = current_user.id
-  @books.save
-  redirect_to books_path
+  @book = Book.new(book_params)
+  @book.user_id = current_user.id
+      if @book.save
+         redirect_to book_path(@book)
+      else
+         @books = Book.all
+         render action: :index
 
+      end
 end
+
 
 def edit
   @book = Book.find(params[:id])
@@ -26,17 +32,24 @@ def edit
 end
 
 def update
+  book = Book.find(params[:id])
+  book.update(book_params)
+  redirect_to book_path(book.id)
 
 end
 
 def destroy
-
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to books_path
 end
 
 
-private
+
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body , :user_id)
   end
+
+
 
 end
--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books.all
+    @books = @user.books
 
   end
 
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
- 
+
   end
 
 
