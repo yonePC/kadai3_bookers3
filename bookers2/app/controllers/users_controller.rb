3fededcc0d1c20fcca9873d389d897a74fc284d2
yonePC <ys.mailaddress0929@gmail.com>
2021-06-22 14:17:15 +0000

    [update] user編集

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -11,8 +11,18 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-
   end
 
+  def update
+    @user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :encrypted_password, :introduction)
+  end
 
 end
