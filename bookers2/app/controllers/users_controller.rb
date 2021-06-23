260e2e91ce9188896f29fc2f85411c999ca44462
yonePC <ys.mailaddress0929@gmail.com>
2021-06-23 09:30:33 +0000

    [update]左側createボタン修正

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -6,11 +6,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @newbook = Book.new
 
   end
 
   def index
     @users = User.all
+    @newbook = Book.new
   end
 
   def edit
