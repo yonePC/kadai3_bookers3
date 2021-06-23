bf27499fa825eaad40696674add27ea4af745e61
yonePC <ys.mailaddress0929@gmail.com>
2021-06-23 09:20:45 +0000

    [update]プロフィール画像

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many:books, dependent: :destroy
-  attachment :image
+  attachment :profile_image
 
   validates :name, presence: true, uniqueness: true, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50 }
