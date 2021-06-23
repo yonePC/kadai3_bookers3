0ee1873b10cce0b60e3d7b224664c5b7b5aa9b78
yonePC <ys.mailaddress0929@gmail.com>
2021-06-23 08:16:30 +0000

    [update]user他人編集防止

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
+  
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -28,6 +32,13 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+     unless @user == current_user
+     redirect_to books_path
+     end
   end
 
 end
